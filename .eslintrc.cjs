53fb9d4afce39ea7181b29d326f904026f440c5b
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-11-01 22:40:00 +0400

    ci: add eslint configuration files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,77 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	root: true,
+	env: {
+		node: true,
+		es6: true,
+	},
+	parserOptions: { ecmaVersion: 8, sourceType: "module" },
+	ignorePatterns: ["node_modules/*"],
+	extends: ["eslint:recommended"],
+	overrides: [
+		{
+			files: ["**/*.ts", "**/*.tsx"],
+			parser: "@typescript-eslint/parser",
+			settings: {
+				react: { version: "detect" },
+				"import/resolver": {
+					typescript: {},
+				},
+			},
+			env: {
+				browser: true,
+				node: true,
+				es6: true,
+			},
+			extends: [
+				"eslint:recommended",
+				"plugin:react/recommended",
+				"plugin:react-hooks/recommended",
+				"plugin:testing-library/react",
+				"eslint-config-prettier",
+			],
+			rules: {
+				"no-restricted-imports": [
+					"error",
+					{
+						patterns: ["@/features/*/*"],
+					},
+				],
+				"linebreak-style": ["error", "unix"],
+				"react/prop-types": "off",
+
+				"import/order": [
+					"error",
+					{
+						groups: [
+							"builtin",
+							"external",
+							"internal",
+							"parent",
+							"sibling",
+							"index",
+							"object",
+						],
+						"newlines-between": "always",
+						alphabetize: { order: "asc", caseInsensitive: true },
+					},
+				],
+				"import/default": "off",
+				"import/no-named-as-default-member": "off",
+				"import/no-named-as-default": "off",
+
+				"react/react-in-jsx-scope": "off",
+
+				"jsx-a11y/anchor-is-valid": "off",
+
+				"@typescript-eslint/no-unused-vars": ["error"],
+
+				"@typescript-eslint/explicit-function-return-type": ["off"],
+				"@typescript-eslint/explicit-module-boundary-types": ["off"],
+				"@typescript-eslint/no-empty-function": ["off"],
+				"@typescript-eslint/no-explicit-any": ["off"],
+
+				"prettier/prettier": ["error", {}, { usePrettierrc: true }],
+			},
+		},
+	],
+};
