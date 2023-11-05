b44458a956aaeae1a367fd241b6eb3f57a6611e9
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-11-06 03:01:51 +0400

    refactor: usePagination to be able to set total pages and current page outside of the hook

--- src/features/pagination/hooks/use-pagination.jsx
+++ src/features/pagination/hooks/use-pagination.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 
 export const usePagination = (totalItems, itemsPerPage) => {
 	const [currentPage, setCurrentPage] = useState(1);
-	const totalPages = Math.ceil(totalItems / itemsPerPage);
+	const [totalPages, setTotalPages] = useState(
+		Math.ceil(totalItems / itemsPerPage),
+	);
 
 	const handlePageChange = (pageNumber) => {
 		setCurrentPage(pageNumber);
 	};
 
-	return { currentPage, totalPages, handlePageChange };
+	return {
+		currentPage,
+		totalPages,
+		handlePageChange,
+		setTotalPages,
+		setCurrentPage,
+	};
 };
