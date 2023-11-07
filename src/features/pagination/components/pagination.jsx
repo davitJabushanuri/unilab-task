bef4f45f2296029dfad780663f0da05daf170cd5
davitJabushanuri <davitjabushanuri95@gmail.com>
2023-11-07 19:13:47 +0400

    feat: improve accessibility of pagination component with landmarks, roles and semantic structure

--- src/features/pagination/components/pagination.jsx
+++ src/features/pagination/components/pagination.jsx
@@ -8,9 +8,11 @@ export const Pagination = ({ totalPages, currentPage, onPageChange }) => {
 	const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
 	return (
-		<div className={styles.container}>
+		<nav aria-label="Pagination" className={styles.container}>
 			<button
-				aria-label="Navigate to the first page"
+				role="link"
+				aria-label="Go to the first page"
+				aria-disabled={currentPage === 1}
 				disabled={currentPage === 1}
 				onClick={() => onPageChange(1)}
 			>
@@ -18,24 +20,39 @@ export const Pagination = ({ totalPages, currentPage, onPageChange }) => {
 			</button>
 
 			<button
-				aria-label="Previous page"
+				role="link"
+				aria-label={`Go to previous page, page ${currentPage - 1}`}
+				aria-disabled={currentPage === 1}
 				disabled={currentPage === 1}
 				onClick={() => onPageChange(currentPage - 1)}
 			>
 				<ArrowLeftIcon />
 			</button>
-			{pageNumbers.map((number) => (
-				<button
-					key={number}
-					className={number === currentPage ? styles.active : ""}
-					onClick={() => onPageChange(number)}
-					tabIndex={number === currentPage ? -1 : 0}
-				>
-					{number}
-				</button>
-			))}
+			{pageNumbers.map((number) => {
+				return currentPage === number ? (
+					<span
+						key={number}
+						className={styles.active}
+						aria-label={`Current page, page ${number}`}
+					>
+						{number}
+					</span>
+				) : (
+					<button
+						role="link"
+						key={number}
+						className={number === currentPage ? styles.active : ""}
+						onClick={() => onPageChange(number)}
+						aria-label={`Go to page ${number}`}
+					>
+						{number}
+					</button>
+				);
+			})}
 			<button
-				aria-label="Next page"
+				role="link"
+				aria-label={`Go to next page, ${currentPage + 1}`}
+				aria-disabled={currentPage === totalPages}
 				disabled={currentPage === totalPages}
 				onClick={() => onPageChange(currentPage + 1)}
 			>
@@ -43,12 +60,14 @@ export const Pagination = ({ totalPages, currentPage, onPageChange }) => {
 			</button>
 
 			<button
-				aria-label="Navigate to the last page"
+				role="link"
+				aria-label="Go to the last page"
+				aria-disabled={currentPage === totalPages}
 				disabled={currentPage === totalPages}
 				onClick={() => onPageChange(totalPages)}
 			>
 				<ArrowsRightIcon />
 			</button>
-		</div>
+		</nav>
 	);
 };
